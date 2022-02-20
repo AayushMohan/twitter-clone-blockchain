4d5e49347d6a045175f8d108d2c711bb11dd0ddb
AayushMohan <aayushmohan1702@gmail.com>
2022-02-20 18:31:50 +0530

    Added Style, Wrapper, Header

--- client/components/home/Feed.js
+++ client/components/home/Feed.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import { BsStars } from 'react-icons/bs'
+
+const style = {
+  wrapper: `flex-[2] border-r border-l border-[#38444d]`,
+  header: `sticky top-0 bg-[#15202b] z-10 p-4 flex justify-between items-center`,
+  headerTitle: `text-xl font-bold`,
+}
 
 const Feed = () => {
-  return <div>Feed</div>
+  return (
+    <div className={`${style.wrapper}`}>
+      <div className={style.header}></div>
+    </div>
+  )
 }
 
 export default Feed
