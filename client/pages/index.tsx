906a52ee7acf3359575e91abbc05db6a23607869
AayushMohan <aayushmohan1702@gmail.com>
2022-02-20 12:45:43 +0530

    Added Styles

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,15 +1,16 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+const styles = {
+  wrapper: `flex justify-center h-screen w-screen select-none bg-[#15202b] text-white`,
+  content: `max-w-[1400px] w-2/3 flex justify-between`,
+}
 
-const Home: NextPage = () => {
+export default function Home() {
   return (
-    <div>
-      <h2></h2>
-      <h2></h2>
-      <h2></h2>
+    <div className={styles.wrapper}>
+      <div className={styles.content}>
+        <h2>Sidebar</h2>
+        <h2>Feed</h2>
+        <h2>Widgets</h2>
+      </div>
     </div>
   )
 }
-
-export default Home
