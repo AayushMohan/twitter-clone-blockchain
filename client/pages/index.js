05e4dfe8f99d18f4a6494974a5cfaf858060c406
AayushMohan <aayushmohan1702@gmail.com>
2022-02-20 13:10:20 +0530

    Added Sidebar Component

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,5 @@
+import Sidebar from '../components/Sidebar'
+
 const styles = {
   wrapper: `flex justify-center h-screen w-screen select-none bg-[#15202b] text-white`,
   content: `max-w-[1400px] w-2/3 flex justify-between`,
@@ -7,7 +9,7 @@ export default function Home() {
   return (
     <div className={styles.wrapper}>
       <div className={styles.content}>
-        <h2>Sidebar</h2>
+        <Sidebar />
         <h2>Feed</h2>
         <h2>Widgets</h2>
       </div>
