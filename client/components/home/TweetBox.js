c914ddc3a832079ed027685aa6b26e77eb69df6c
AayushMohan <aayushmohan1702@gmail.com>
2022-02-20 19:43:43 +0530

    Added Tweet Box

--- client/components/home/TweetBox.js
+++ client/components/home/TweetBox.js
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const style = {
+  wrapper: `px-4 flex flex-row border-b border-[#38444d] pb-4`,
+  tweetBoxLeft: `mr-4`,
+  tweetBoxRight: `flex-1`,
+  profileImage: `height-12 w-12 rounded-full`,
+  inputField: `w-full h-full outline-none bg-transparent text-lg`,
+  formLowerContainer: `flex`,
+  iconsContainer: `text-[#1d9bf0] flex flex-1 items-center`,
+  icon: `mr-2`,
+  submitGeneral: `px-6 py-2 rounded-3xl font-bold`,
+  inactiveSubmit: `bg-[#196195] text-[#95999e]`,
+  activeSubmit: `bg-[#1d9bf0] text-white`,
+}
 
 const TweetBox = () => {
-  return <div>TweetBox</div>
+  const [tweetMessage, setTweetMessage] = useState('')
+
+  console.log(tweetMessage)
+
+  return (
+    <div className={style.wrapper}>
+      <div className={style.tweetBoxLeft}>
+        <img
+          src="https://avatars.githubusercontent.com/u/66319691?v=4"
+          alt="Profile Image"
+          className={style.profileImage}
+        />
+      </div>
+      <div className={style.tweetBoxRight}>
+        <form>
+          <textarea
+            className={style.inputField}
+            placeholder="What's happening?"
+            value={tweetMessage}
+            onChange={(e) => setTweetMessage(e.target.value)}
+          ></textarea>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default TweetBox
