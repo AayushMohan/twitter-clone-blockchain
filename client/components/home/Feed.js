c0312c46c49e3be3a546f3260fe76c847ead6505
AayushMohan <aayushmohan1702@gmail.com>
2022-02-25 19:28:29 +0530

    Added Tweet And Profile Tweets

--- client/components/home/Feed.js
+++ client/components/home/Feed.js
@@ -8,41 +8,6 @@ const style = {
   headerTitle: `text-xl font-bold`,
 }
 
-const tweets = [
-  {
-    displayName: 'Aayush',
-    userName: '0x9740D80A3bdFF2bE7f6308B5f49C42D711a8d531',
-    avatar: 'https://avatars.githubusercontent.com/u/66319691?v=4',
-    text: 'GM',
-    isProfileImageNFT: false,
-    timeStamp: '2022-02-01T12:00:00.000Z', // This is how Sanity stores timestamp
-  },
-  {
-    displayName: 'Aayush',
-    userName: '0x9740D80A3bdFF2bE7f6308B5f49C42D711a8d531',
-    avatar: 'https://avatars.githubusercontent.com/u/66319691?v=4',
-    text: 'GM',
-    isProfileImageNFT: false,
-    timeStamp: '2022-01-01T12:00:00.000Z', // This is how Sanity stores timestamp
-  },
-  {
-    displayName: 'Aayush',
-    userName: '0x9740D80A3bdFF2bE7f6308B5f49C42D711a8d531',
-    avatar: 'https://avatars.githubusercontent.com/u/66319691?v=4',
-    text: 'GM',
-    isProfileImageNFT: false,
-    timeStamp: '2021-10-01T12:00:00.000Z', // This is how Sanity stores timestamp
-  },
-  {
-    displayName: 'Aayush',
-    userName: '0x9740D80A3bdFF2bE7f6308B5f49C42D711a8d531',
-    avatar: 'https://avatars.githubusercontent.com/u/66319691?v=4',
-    text: 'GM',
-    isProfileImageNFT: false,
-    timeStamp: '2021-11-01T12:00:00.000Z', // This is how Sanity stores timestamp
-  },
-]
-
 const Feed = () => {
   return (
     <div className={style.wrapper}>
--- client/components/profile/ProfileTweets.js
+++ client/components/profile/ProfileTweets.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Post from '../Post'
 
 const style = {
   wrapper: `no-scrollbar`,
@@ -6,8 +7,49 @@ const style = {
   headerTitle: `text-xl font-bold`,
 }
 
+const tweets = [
+  {
+    displayName: 'Aayush',
+    userName: '0x9740D80A3bdFF2bE7f6308B5f49C42D711a8d531',
+    avatar: 'https://avatars.githubusercontent.com/u/66319691?v=4',
+    text: 'GM',
+    isProfileImageNFT: false,
+    timeStamp: '2022-02-01T12:00:00.000Z', // This is how Sanity stores timestamp
+  },
+  {
+    displayName: 'Aayush',
+    userName: '0x9740D80A3bdFF2bE7f6308B5f49C42D711a8d531',
+    avatar: 'https://avatars.githubusercontent.com/u/66319691?v=4',
+    text: 'GM',
+    isProfileImageNFT: false,
+    timeStamp: '2022-01-01T12:00:00.000Z', // This is how Sanity stores timestamp
+  },
+  {
+    displayName: 'Aayush',
+    userName: '0x9740D80A3bdFF2bE7f6308B5f49C42D711a8d531',
+    avatar: 'https://avatars.githubusercontent.com/u/66319691?v=4',
+    text: 'GM',
+    isProfileImageNFT: false,
+    timeStamp: '2021-10-01T12:00:00.000Z', // This is how Sanity stores timestamp
+  },
+  {
+    displayName: 'Aayush',
+    userName: '0x9740D80A3bdFF2bE7f6308B5f49C42D711a8d531',
+    avatar: 'https://avatars.githubusercontent.com/u/66319691?v=4',
+    text: 'GM',
+    isProfileImageNFT: false,
+    timeStamp: '2021-11-01T12:00:00.000Z', // This is how Sanity stores timestamp
+  },
+]
+
 const ProfileTweets = () => {
-  return <div>ProfileTweets</div>
+  return (
+    <div className={style.wrapper}>
+      {tweets.map((tweet, index) => (
+        <Post />
+      ))}
+    </div>
+  )
 }
 
 export default ProfileTweets
