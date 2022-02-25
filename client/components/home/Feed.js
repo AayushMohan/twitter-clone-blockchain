e161938133664ae8898dd64f6b6a6137b7acc03a
AayushMohan <aayushmohan1702@gmail.com>
2022-02-25 14:22:26 +0530

    Fixed Few Minor Bugs

--- client/components/home/Feed.js
+++ client/components/home/Feed.js
@@ -3,7 +3,7 @@ import Post from '../Post'
 import TweetBox from './TweetBox'
 
 const style = {
-  wrapper: `flex-[2] border-r border-l border-[#38444d]`,
+  wrapper: `flex-[2] border-r border-l border-[#38444d] overflow-y-scroll`,
   header: `sticky top-0 bg-[#15202b] z-10 p-4 flex justify-between items-center`,
   headerTitle: `text-xl font-bold`,
 }
