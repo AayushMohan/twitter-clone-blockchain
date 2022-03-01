c7f210349210cbcc6122d6fc0e3986e9cd8d5c5c
AayushMohan <aayushmohan1702@gmail.com>
2022-03-01 14:18:57 +0530

    Added Constants and Profile Image NFT

---
+++ client/lib/constants.js
@@ -0,0 +1,4 @@
+import contractArtifact from './ProfileImageNfts.json'
+
+export const contractAddress = '0x9740D80A3bdFF2bE7f6308B5f49C42D711a8d531'
+export const contractABI = contractArtifact.abi
