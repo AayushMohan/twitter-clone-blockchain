ce51f0d554ac85797d43cdf5db4025caf70159a8
AayushMohan <aayushmohan1702@gmail.com>
2022-03-02 13:59:32 +0530

    Added MetaMask Authentication

--- client/context/TwitterContext.js
+++ client/context/TwitterContext.js
@@ -1,18 +1,21 @@
 import { Children, createContext, useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 
 export const TwitterContext = createContext()
 
 export const TwitterProvider = ({ children }) => {
-  const [appStatus, setAppStatus]  = useState('loading')
+  const [appStatus, setAppStatus] = useState('loading')
   const [currentAccount, setCurrentAccount] = useState('')
+
+  const router = useRouter()
 
   useEffect(() => {
     checkIfWalletIsConnected()
   }, [])
 
-   const checkIfWalletIsConnected = async () => {
-    if (!window.ethereum) return setAppStatus('noMetaMask') 
-    try{
+  const checkIfWalletIsConnected = async () => {
+    if (!window.ethereum) return setAppStatus('noMetaMask')
+    try {
       const addressArray = await window.ethereum.request({
         method: 'eth_accounts',
       })
@@ -22,25 +25,39 @@ export const TwitterProvider = ({ children }) => {
         setAppStatus('connected')
         setCurrentAccount(addressArray[0])
       } else {
-        // Not Connected
+        router.push('/')
         setAppStatus('notConnected')
       }
     } catch (error) {
       console.log(error)
     }
-   }
+  }
 
-   const connectToWallet = async () => {
-     if(!window.ethereum) return setAppStatus('noMetaMask')
+  const connectToWallet = async () => {
+    if (!window.ethereum) return setAppStatus('noMetaMask')
 
-     try{
+    try {
+      setAppStatus('loading')
 
-     } catch (error) {
-       setAppStatus('error')
-     }
+      const addressArray = await window.ethereum.request({
+        method: 'eth_accounts',
+      })
 
-   }
-   return( 
-   <TwitterContext.Provider value={}>{children}</TwitterContext.Provider>
+      if (addressArray.length > 0) {
+        setCurrentAccount(addressArray[0])
+      } else {
+        router.push('/')
+        setAppStatus('notConnected')
+      }
+    } catch (error) {
+      setAppStatus('error')
+    }
+  }
+  return (
+    <TwitterContext.Provider
+      value={{ appStatus, currentAccount, connectToWallet }}
+    >
+      {children}
+    </TwitterContext.Provider>
   )
 }
