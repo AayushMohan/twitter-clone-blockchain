5e2770c5b43ce8b0601119b4db68d053a0549151
AayushMohan <aayushmohan1702@gmail.com>
2022-03-03 21:27:15 +0530

    Added Tweet Schema

--- studio/schemas/tweetSchema.js
+++ studio/schemas/tweetSchema.js
@@ -1,1 +1,23 @@
-export const tweetSchema = [];
+export const tweetSchema = {
+  name: "tweets",
+  title: "Tweets",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "timestamp",
+      type: "datetime",
+      title: "Timestamp",
+    },
+    {
+      name: "author",
+      title: "Author",
+      type: "reference",
+      to: [{ type: "users" }],
+    },
+  ],
+};
--- studio/schemas/userSchema.js
+++ studio/schemas/userSchema.js
@@ -1,1 +1,1 @@
-export const userSchema = [];
+export const userSchema = {};
